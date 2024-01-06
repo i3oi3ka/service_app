cae37214a1e25947947d32e6ba17c6b77422dedc
i3oi3ka <i3oi3ka@ukr.net>
2024-01-06 20:28:08 +0200

    feat sql queryset

--- service/services/views.py
+++ service/services/views.py
@@ -9,7 +9,7 @@ from services.serializers import SubscriptionSerializer
 
 class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().prefetch_related(
-        Prefetch('client',
-                 queryset=Client.objects.all().select_related('user').only('company_name', 'user__email'))
+        'plan',
+        Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name', 'user__email'))
     )
     serializer_class = SubscriptionSerializer
